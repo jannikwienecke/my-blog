6118120b88f19f66c9a921a08dbdfb4240186bb8
Jannik Wienecke <jannik.wienecke@sirum.de>
2023-01-14 09:45:19 +0100

    add typed. menu

---
+++ src/layouts/type.js
@@ -0,0 +1,16 @@
+import Typed from "typed.js";
+
+new Typed(".animated_title", {
+  strings: [
+    "Jannik Wienecke",
+    "Web Developer",
+    "Freelancer",
+    "App Developer",
+    "Consultant",
+  ],
+  loop: true,
+  smartBackspace: false,
+  typeSpeed: 40,
+  startDelay: 400,
+  backDelay: 3e3,
+});
